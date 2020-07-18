3d80973ce609b4fa02f449456610f322c5a5b728
aaguiar96 <up201403474@fe.up.pt>
2020-07-18 15:51:15 +0100

    #207 - Evaluation program allows to write and read evaluation data from file OR annotate and save it

--- atom_evaluation/scripts/range_sensor_to_camera_evaluation.py
+++ atom_evaluation/scripts/range_sensor_to_camera_evaluation.py
@@ -14,11 +14,39 @@ import numpy as np
 import cv2
 import argparse
 import OptimizationUtils.utilities as opt_utilities
+from copy import deepcopy
+from colorama import Style, Fore
 
 
 # -------------------------------------------------------------------------------
 # --- FUNCTIONS
 # -------------------------------------------------------------------------------
+
+def walk(node):
+    for key, item in node.items():
+        if isinstance(item, dict):
+            walk(item)
+        else:
+            if isinstance(item, np.ndarray) and key == 'data':  # to avoid saving images in the json
+                del node[key]
+
+            elif isinstance(item, np.ndarray):
+                node[key] = item.tolist()
+            pass
+
+
+# Save to json file
+def createJSONFile(output_file, input):
+    D = deepcopy(input)
+    walk(D)
+
+    print("Saving the json output file to " + str(output_file) + ", please wait, it could take a while ...")
+    f = open(output_file, 'w')
+    json.encoder.FLOAT_REPR = lambda f: ("%.6f" % f)  # to get only four decimal places on the json file
+    print >> f, json.dumps(D, indent=2, sort_keys=True)
+    f.close()
+    print("Completed.")
+
 
 def rangeToImage(collection, ss, ts, tf):
     # -- Convert limit points from velodyne to camera frame
@@ -37,58 +65,6 @@ def rangeToImage(collection, ss, ts, tf):
     pts_in_image, _, _ = opt_utilities.projectToCamera(K, D, w, h, points_in_cam[0:3, :])
 
     return pts_in_image
-
-
-def getPatternCorners(dataset, collection, ts):
-    # - Get pattern dimensions
-    nx = dataset['calibration_config']['calibration_pattern']['dimension']['x']
-    ny = dataset['calibration_config']['calibration_pattern']['dimension']['y']
-    square = dataset['calibration_config']['calibration_pattern']['size']
-    if type(dataset['calibration_config']['calibration_pattern']['border_size']) is dict:
-        border_x = dataset['calibration_config']['calibration_pattern']['border_size']['x']
-        border_y = dataset['calibration_config']['calibration_pattern']['border_size']['y']
-    else:
-        border_x = border_y = dataset['calibration_config']['calibration_pattern']['border_size']
-
-    # - Get pattern detected corners
-    objp = np.zeros((nx * ny, 3), np.float32)
-    objp[:, :2] = square * np.mgrid[0:nx, 0:ny].T.reshape(-1, 2)
-
-    corners = np.zeros((len(collection['labels'][ts]['idxs']), 1, 2), dtype=np.float)
-    ids = range(0, len(collection['labels'][ts]['idxs']))
-    for idx, point in enumerate(collection['labels'][ts]['idxs']):
-        corners[idx, 0, 0] = point['x']
-        corners[idx, 0, 1] = point['y']
-        ids[idx] = point['id']
-
-    # - Sensor to pattern transformation
-    w, h = collection['data'][ts]['width'], collection['data'][ts]['height']
-    K = np.ndarray((3, 3), buffer=np.array(dataset['sensors'][ts]['camera_info']['K']), dtype=np.float)
-    D = np.ndarray((5, 1), buffer=np.array(dataset['sensors'][ts]['camera_info']['D']), dtype=np.float)
-
-    ret, rvecs, tvecs = cv2.solvePnP(objp[ids], np.array(corners, dtype=np.float32), K, D)
-    sensor_to_pattern = opt_utilities.traslationRodriguesToTransform(tvecs, rvecs)
-
-    # - Pattern extrema corners to image
-    top_left = np.array([-square - border_x, -square - border_y, 0, 1])
-    top_right = np.array([nx * square + border_x, -square - border_y, 0, 1])
-    bottom_right = np.array([nx * square + border_x, ny * square + border_y, 0, 1])
-    bottom_left = np.array([-square - border_x, ny * square + border_y, 0, 1])
-
-    top_left_on_cam = np.dot(sensor_to_pattern, np.transpose(top_left))
-    top_right_on_cam = np.dot(sensor_to_pattern, np.transpose(top_right))
-    bottom_left_on_cam = np.dot(sensor_to_pattern, np.transpose(bottom_left))
-    bottom_right_on_cam = np.dot(sensor_to_pattern, np.transpose(bottom_right))
-
-    corners_on_cam = np.zeros((4, 4), dtype=np.float32)
-    corners_on_cam[:, 0] = top_left_on_cam
-    corners_on_cam[:, 1] = top_right_on_cam
-    corners_on_cam[:, 2] = bottom_left_on_cam
-    corners_on_cam[:, 3] = bottom_right_on_cam
-
-    corners_on_image, _, _ = opt_utilities.projectToCamera(K, D, w, h, corners_on_cam[0:3, :])
-
-    return corners_on_image
 
 
 mouseX, mouseY = 0, 0
@@ -131,23 +107,46 @@ if __name__ == "__main__":
     ap.add_argument("-json", "--json_file", help="Json file containing input dataset.", type=str, required=True)
     ap.add_argument("-ss", "--source_sensor", help="Source transformation sensor.", type=str, required=True)
     ap.add_argument("-ts", "--target_sensor", help="Target transformation sensor.", type=str, required=True)
-    ap.add_argument("-si", "--show_images", help="Shows images.", action='store_true', default=False)
-    ap.add_argument("-at", "--corners_auto", help="Automatic corner detection.", action='store_true', default=False)
+    ap.add_argument("-si", "--show_images", help="If true the script shows images.", action='store_true', default=False)
+    ap.add_argument("-ef", "--eval_file", help="Path to file to read and/or write the evalutation data.", type=str,
+                    required=True)
+    ap.add_argument("-ua", "--use_annotation", help="If true, the limit points will be manually annotated.",
+                    action='store_true', default=False)
 
     # - Save args
     args = vars(ap.parse_args())
     source_sensor = args['source_sensor']
     target_sensor = args['target_sensor']
     show_images = args['show_images']
-    auto = args['corners_auto']
+    eval_file = args['eval_file']
+    use_annotation = args['use_annotation']
 
     # ---------------------------------------
-    # --- INITIALIZATION Read data from file
+    # --- INITIALIZATION Read calibration data from file
     # ---------------------------------------
     # Loads a json file containing the calibration
     json_file = args['json_file']
     f = open(json_file, 'r')
     dataset = json.load(f)
+
+    # ---------------------------------------
+    # --- INITIALIZATION Read evaluation data from file ---> if desired <---
+    # ---------------------------------------
+    if use_annotation is False:
+        # Loads a json file containing the evaluation data
+        f = open(eval_file, 'r')
+        eval_data = json.load(f)
+    else:
+        print(Fore.BLUE + "  Annotation tool intructions:")
+        print(Fore.GREEN + "   - To add a point to a class: click + 's'")
+        print(Fore.GREEN + "   - To change class: 'p'")
+        print(Fore.GREEN + "   - To stop the annotation anytime: 'c'")
+        print(Fore.GREEN + "   - It ends when you end annotating the fourth class (four times 'p')")
+        print(Fore.WHITE)
+
+    # Declare output dict to save the evaluation data if desired
+    output_dict = {}
+    output_dict['ground_truth_pts'] = {}
 
     from_frame = dataset['calibration_config']['sensors'][target_sensor]['link']
     to_frame = dataset['calibration_config']['sensors'][source_sensor]['link']
@@ -159,43 +158,45 @@ if __name__ == "__main__":
         pts_in_image = rangeToImage(collection, source_sensor, target_sensor, vel2cam)
 
         # ---------------------------------------
-        # --- Get pattern corners (auto)
+        # --- Get evaluation data for current collection
         # ---------------------------------------
         filename = os.path.dirname(args['json_file']) + '/' + collection['data'][target_sensor]['data_file']
         image = cv2.imread(filename)
-        if auto == True:
-            corners_on_image = getPatternCorners(dataset, collection, target_sensor)
-            # ---------------------------------------
-            # --- Drawing ...
-            # ---------------------------------------
-            if show_images == True:
-                for idx in range(0, corners_on_image.shape[1]):
-                    image = cv2.circle(image, (int(corners_on_image[0, idx]), int(corners_on_image[1, idx])), 5,
-                                       (0, 255, 0), -1)
+        if use_annotation is False:
+            limits_on_image = eval_data['ground_truth_pts'][collection_key]
         else:
-            corners_on_image = annotateLimits(image)
-            for i, pts in corners_on_image.items():
-                pts = np.array(pts)
-                if (pts.size == 0):
-                    continue
+            limits_on_image = annotateLimits(image)
 
-                x = pts[:, 0]
-                y = pts[:, 1]
-                coefficients = np.polyfit(x, y, 3)
-                poly = np.poly1d(coefficients)
-                new_x = np.linspace(x[0], x[-1])
-                new_y = poly(new_x)
-                if show_images == True:
-                    for idx in range(0, len(new_x)):
-                        image = cv2.circle(image, (int(new_x[idx]), int(new_y[idx])), 5, (0, 0, 0), -1)
+        output_dict['ground_truth_pts'][collection_key] = {}
+        for i, pts in limits_on_image.items():
+            pts = np.array(pts)
+            if (pts.size == 0):
+                continue
 
+            x = pts[:, 0]
+            y = pts[:, 1]
+            coefficients = np.polyfit(x, y, 3)
+            poly = np.poly1d(coefficients)
+            new_x = np.linspace(x[0], x[-1])
+            new_y = poly(new_x)
+            if show_images == True:
+                for idx in range(0, len(new_x)):
+                    image = cv2.circle(image, (int(new_x[idx]), int(new_y[idx])), 3, (0, 0, 255), -1)
+
+            output_dict['ground_truth_pts'][collection_key][i] = []
+            for idx in range(0, len(new_x)):
+                output_dict['ground_truth_pts'][collection_key][i].append([new_x[idx], new_y[idx]])
 
         # ---------------------------------------
         # --- Drawing ...
         # ---------------------------------------
-        if show_images == True:
+        if show_images is True:
             for idx in range(0, pts_in_image.shape[1]):
                 image = cv2.circle(image, (int(pts_in_image[0, idx]), int(pts_in_image[1, idx])), 5, (255, 0, 0), -1)
 
             cv2.imshow("Lidar to Camera reprojection - collection " + str(collection_key), image)
             cv2.waitKey()
+
+    # Save evaluation data
+    if use_annotation is True:
+        createJSONFile(eval_file, output_dict)
