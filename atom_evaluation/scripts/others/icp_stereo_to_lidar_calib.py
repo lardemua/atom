92dddeafb12d22be7aa606bcba42085d24cd21d7
andre.s.aguiar <andre.s.aguiar@inesctec.pt>
2020-09-28 11:17:44 +0100

    #130 implemented

--- atom_evaluation/scripts/others/icp_stereo_to_lidar_calib.py
+++ atom_evaluation/scripts/others/icp_stereo_to_lidar_calib.py
@@ -8,10 +8,14 @@ Stereo calibration from opencv
 # --- IMPORTS
 # -------------------------------------------------------------------------------
 
+import rospy
+import std_msgs.msg
+import sensor_msgs.point_cloud2 as pc2
 import atom_core.atom
 import numpy as np
 import json
 import argparse
+from sensor_msgs.msg import PointCloud2
 from pcl import PointCloud
 
 
@@ -30,6 +34,7 @@ def load(path, format=None):
         raise IOError("error while loading pointcloud from %r (format=%r)"
                       % (path, format))
     return p
+
 
 def _encode(path):
     # Encode path for use in C++.
@@ -50,23 +55,42 @@ def _infer_format(path, format):
     raise ValueError("Could not determine file format from pathname %s" % path)
 
 
+def transformPCL(input_pcl, tf):
+    np_pcl = np.asarray(input_pcl)
+
+    transformed_pcl = []
+    for idx in range(0, np_pcl.shape[0]):
+        pt = np.zeros((4), np.float32)
+        pt[0:3] = np.array(np_pcl[idx, :])
+        pt[3] = 1
+
+        tf_pt = np.dot(tf, pt.transpose())[0:3]
+        transformed_pcl.append(tf_pt)
+
+    return np.array(transformed_pcl)
+
+
 if __name__ == "__main__":
+    rospy.init_node('icp_aligner', anonymous=True)
+
     ap = argparse.ArgumentParser()
     ap.add_argument("-json", "--json_file", help="Json file containing initial guess estimate.", type=str,
                     required=True)
-    ap.add_argument("-ss", "--source_sensor", help="Source transformation sensor.", type=str, required=True)
-    ap.add_argument("-ts", "--target_sensor", help="Target transformation sensor.", type=str, required=True)
-    ap.add_argument("-fpcl", "--first_pcl", help="First point cloud from the two to be aligned", type=str,
+    ap.add_argument("-sf", "--source_frame", help="Source frame link.", type=str, required=True)
+    ap.add_argument("-tf", "--target_frame", help="Target frame link.", type=str, required=True)
+    ap.add_argument("-rf", "--reference_frame", help="Reference frame link.", type=str, required=True)
+    ap.add_argument("-spcl", "--source_pcl", help="Source point cloud from the two to be aligned", type=str,
                     required=True)
-    ap.add_argument("-spcl", "--second_pcl", help="Second point cloud from the two to be aligned", type=str,
+    ap.add_argument("-tpcl", "--target_pcl", help="Target point cloud from the two to be aligned", type=str,
                     required=True)
 
     # Save args
     args = vars(ap.parse_args())
-    fpcl_path = args['first_pcl']
-    spcl_path = args['second_pcl']
-    source_sensor = args['source_sensor']
-    target_sensor = args['target_sensor']
+    tpcl_path = args['target_pcl']
+    spcl_path = args['source_pcl']
+    target_frame = args['target_frame']
+    source_frame = args['source_frame']
+    reference_frame = args['reference_frame']
 
     # Load input json file
     json_file = args['json_file']
@@ -74,28 +98,52 @@ if __name__ == "__main__":
     dataset = json.load(f)
 
     # Load point clouds from files
-    fpcl = load(fpcl_path)
+    tpcl = load(tpcl_path)
     spcl = load(spcl_path)
 
     # Get initial guess
     selected_collection_key = dataset['collections'].keys()[0]
-    from_frame = dataset['calibration_config']['sensors'][target_sensor]['link']
-    to_frame = dataset['calibration_config']['sensors'][source_sensor]['link']
-    vel2cam = atom_core.atom.getTransform(from_frame, to_frame,
-                                          dataset['collections'][selected_collection_key]['transforms'])
+    source2base = atom_core.atom.getTransform(source_frame, reference_frame,
+                                              dataset['collections'][selected_collection_key]['transforms'])
+    target2base = atom_core.atom.getTransform(target_frame, reference_frame,
+                                              dataset['collections'][selected_collection_key]['transforms'])
 
-    # Transform source point cloud using initial guess
-    fpcl_h = np.zeros((fpcl.size, 4), np.float32)
-    fpcl_h[:, 0:3] = fpcl
-    fpcl_h[:, 3] = 1
-    fpcl_h = np.dot(fpcl_h, vel2cam)
-    fpcl_h = fpcl_h[:, 0:3]
-    fpcl = PointCloud(fpcl_h.astype(np.float32))
+    # Transform source and target point clouds to world using the initial guess
+    tpcl_base = transformPCL(tpcl, np.linalg.inv(target2base))
+    tpcl_base = PointCloud(tpcl_base.astype(np.float32))
+    spcl_base = transformPCL(spcl, np.linalg.inv(source2base))
+    spcl_base = PointCloud(spcl_base.astype(np.float32))
 
     # Apply icp alignment
-    print ('Starting ICP alignemnt ...')
-    icp = fpcl.make_IterativeClosestPoint()
-    converged, transf, estimate, fitness = icp.icp(fpcl, spcl)
+    print ('\n-----\nStarting ICP alignemnt ...\n-----\n')
+    icp = tpcl_base.make_IterativeClosestPoint()
+    converged, transf, estimate, fitness = icp.icp(tpcl_base, spcl_base)
 
     print('Has converged:' + str(converged) + ', score: ' + str(fitness))
     print('Result:\n' + str(transf))
+
+    print ('\n-----\nPublishing point clouds. You can visualize them on rviz.\n-----\n')
+    # Declare pcl publishers
+    source_pcl_pub = rospy.Publisher("/source_pcl", PointCloud2, queue_size=1)
+    target_pcl_pub = rospy.Publisher("/target_pcl", PointCloud2, queue_size=1)
+    aligned_pcl_pub = rospy.Publisher("/aligned_pcl", PointCloud2, queue_size=1)
+
+    # Convert point clouds to numpy arrays
+    np_tpcl = np.asarray(tpcl_base)
+    np_spcl = np.asarray(spcl_base)
+    np_aligned = np.asarray(estimate)
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        h = std_msgs.msg.Header()
+        h.frame_id = 'velodyne'
+        h.stamp = rospy.Time.now()
+
+        tpcl_cloud = pc2.create_cloud_xyz32(h, np_tpcl)
+        spcl_cloud = pc2.create_cloud_xyz32(h, np_spcl)
+        aligned_cloud = pc2.create_cloud_xyz32(h, np_aligned)
+
+        source_pcl_pub.publish(spcl_cloud)
+        target_pcl_pub.publish(tpcl_cloud)
+        aligned_pcl_pub.publish(aligned_cloud)
+
+        rate.sleep()
