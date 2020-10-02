171e66fadfb093937cfb1443a756f67a46b712e2
andre.s.aguiar <andre.s.aguiar@inesctec.pt>
2020-10-02 17:16:16 +0100

    #221 implemented

--- atom_evaluation/scripts/others/cv_stereo_calib.py
+++ atom_evaluation/scripts/others/cv_stereo_calib.py
@@ -13,11 +13,13 @@ import cv2
 import argparse
 import json
 import os
+import tf
 
 from colorama import Style, Fore
 from collections import OrderedDict
 from atom_evaluation.utilities import atomicTfFromCalibration
 from atom_core.atom import getTransform
+from atom_core.dataset_io import saveResultsJSON
 
 
 def cvStereoCalibrate(objp, images_right, images_left):
@@ -28,57 +30,25 @@ def cvStereoCalibrate(objp, images_right, images_left):
     objpoints = []  # 3d point in real world space
     imgpoints_l = []  # 2d points in image plane.
     imgpoints_r = []  # 2d points in image plane.
-    allids_l = []  # detect corner ids for left camera
-    allids_r = []  # detect corner ids for right camera
 
-    for i, fname in enumerate(images_right):
-        # Read stereo images
-        img_l = cv2.imread(images_left[i])
-        img_r = cv2.imread(images_right[i])
-
-        gray_l = cv2.cvtColor(img_l, cv2.COLOR_BGR2GRAY)
-        gray_r = cv2.cvtColor(img_r, cv2.COLOR_BGR2GRAY)
-
+    for collection_key, collection in dataset['collections'].items():
         # Find the chess board corners
         n_points = int(dataset['calibration_config']['calibration_pattern']['dimension']['x']) * \
                    int(dataset['calibration_config']['calibration_pattern']['dimension']['y'])
         image_points_r = np.ones((n_points, 2), np.float32)
         image_points_l = np.ones((n_points, 2), np.float32)
 
-        for idx, point in enumerate(dataset['collections'][collection_key]['labels'][right_camera]['idxs']):
+        for idx, point in enumerate(collection['labels'][right_camera]['idxs']):
             image_points_r[idx, 0] = point['x']
             image_points_r[idx, 1] = point['y']
 
-        for idx, point in enumerate(dataset['collections'][collection_key]['labels'][left_camera]['idxs']):
+        for idx, point in enumerate(collection['labels'][left_camera]['idxs']):
             image_points_l[idx, 0] = point['x']
             image_points_l[idx, 1] = point['y']
 
         imgpoints_l.append(image_points_l)
         imgpoints_r.append(image_points_r)
         objpoints.append(objp)
-
-        # Show detections if desired
-        if show_images:
-            for point in image_points_l:
-                cv2.drawMarker(img_l, tuple(point[0]), (0, 0, 255), cv2.MARKER_CROSS, 14)
-                cv2.circle(img_l, tuple(point[0]), 7, (0, 255, 0), lineType=cv2.LINE_AA)
-
-            cv2.imshow(images_left[i], img_l)
-            cv2.waitKey(500)
-
-            for point in image_points_r:
-                cv2.drawMarker(img_r, tuple(point[0]), (0, 0, 255), cv2.MARKER_CROSS, 14)
-                cv2.circle(img_r, tuple(point[0]), 7, (0, 255, 0), lineType=cv2.LINE_AA)
-
-            cv2.imshow(images_right[i], img_l)
-            cv2.waitKey(500)
-
-        img_shape = img_l.shape[0:2]
-
-    # Individual intrinsic calibration
-    objpoints = np.array(objpoints)
-    imgpoints_l = np.array(imgpoints_l)
-    imgpoints_r = np.array(imgpoints_r)
 
     # ---------------------------------------
     # --- Get intrinsic data for both sensors
@@ -99,17 +69,20 @@ def cvStereoCalibrate(objp, images_right, images_left):
     K_l[2, :] = dataset['sensors'][left_camera]['camera_info']['K'][6:9]
     D_l[:, 0] = dataset['sensors'][left_camera]['camera_info']['D'][0:5]
 
+    height = dataset['sensors'][right_camera]['camera_info']['height']
+    width = dataset['sensors'][right_camera]['camera_info']['width']
+    image_size = (height, width)
 
     print ('\n---------------------\n Starting stereo calibration ...')
 
     # Extrinsic stereo calibration
     stereocalib_criteria = (cv2.TERM_CRITERIA_MAX_ITER +
                             cv2.TERM_CRITERIA_EPS, 100, 1e-5)
-    flags = (cv2.CALIB_FIX_INTRINSIC)
+    flags = (cv2.CALIB_USE_INTRINSIC_GUESS)
 
     ret, K_l, D_l, K_r, D_r, R, T, E, F = cv2.stereoCalibrate(objpoints, imgpoints_l,
                                                               imgpoints_r, K_l, D_l, K_r,
-                                                              D_r, img_shape,
+                                                              D_r, image_size,
                                                               criteria=stereocalib_criteria, flags=flags)
 
     print ('\n---------------------\n Done!\n\n------\nCalibration results:\n------\n')
@@ -187,11 +160,17 @@ if __name__ == '__main__':
 
     # Compute OpenCV stereo calibration
     calib_model = cvStereoCalibrate(objp, paths_r, paths_l)
+    R = calib_model['R']
+    t = calib_model['T']
+    K_r = calib_model['K_r']
+    D_r = calib_model['D_r']
+    K_l = calib_model['K_l']
+    D_l = calib_model['D_l']
 
     # Extract homogeneous transformation between cameras
     calib_tf = np.zeros((4,4), np.float32)
-    calib_tf[0:3, 0:3] = calib_model['R']
-    calib_tf[0:3, 3] = calib_model['T'].ravel()
+    calib_tf[0:3, 0:3] = R
+    calib_tf[0:3, 3] = t.ravel()
     calib_tf[3, 0:3] = 0
     calib_tf[3, 3] = 1
 
@@ -202,8 +181,27 @@ if __name__ == '__main__':
 
     res = atomicTfFromCalibration(dataset, right_camera, left_camera, calib_tf)
 
+    # Re-write atomic transformation to the json file ...
+    dataset['sensors'][right_camera]['camera_info']['K'][0:3] = K_r[0, :]
+    dataset['sensors'][right_camera]['camera_info']['K'][3:6] = K_r[1, :]
+    dataset['sensors'][right_camera]['camera_info']['K'][6:9] = K_r[2, :]
+
+    dataset['sensors'][left_camera]['camera_info']['K'][0:3] = K_l[0, :]
+    dataset['sensors'][left_camera]['camera_info']['K'][3:6] = K_l[1, :]
+    dataset['sensors'][left_camera]['camera_info']['K'][6:9] = K_l[2, :]
+
+    dataset['sensors'][right_camera]['camera_info']['D'][0:5] = D_r[:, 0]
+    dataset['sensors'][left_camera]['camera_info']['D'][0:5] = D_l[:, 0]
+
     child_link = dataset['calibration_config']['sensors'][left_camera]['child_link']
     parent_link = dataset['calibration_config']['sensors'][left_camera]['parent_link']
+    frame = parent_link + '-' + child_link
+    quat = tf.transformations.quaternion_from_matrix(res)
+    for collection_key, collection in dataset['collections'].items():
+        dataset['collections'][collection_key]['transforms'][frame]['quat'] = quat
+        dataset['collections'][collection_key]['transforms'][frame]['trans'] = res[0:3, 3]
 
-    # Re-write atomic transformation to the json file ...
-
+    # Save results to a json file
+    print (quat)
+    filename_results_json = os.path.dirname(json_file) + '/cv_calibration.json'
+    saveResultsJSON(filename_results_json, dataset)
