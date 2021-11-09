e4a0669cd53f2784646bde23a59609897b71537b
Miguel Oliveira <m.riem.oliveira@gmail.com>
2021-11-09 19:15:05 +0000

    #326 first attempt.

--- atom_core/src/atom_core/utilities.py
+++ atom_core/src/atom_core/utilities.py
@@ -7,58 +7,65 @@ Reads a set of data and labels from a group of sensors in a json file and calibr
 
 # 3rd-party
 import math
+import subprocess
+
+import readchar
 from colorama import Fore, Style
 
 # 3rd-party
 from rospy_message_converter import message_converter
 from pynput import keyboard
-
+from signal import setitimer, signal, SIGALRM, ITIMER_REAL
 
 # from open3d import * # This cannot be used. It itereferes with the Image for getMessageTypeFromTopic(topic):
-
 
 
 # -------------------------------------------------------------------------------
 # --- FUNCTIONS
 # -------------------------------------------------------------------------------
 
-# def alarm_handler(signum, frame):
-#     raise TimeoutExpired
-#
-# def input_with_timeout(prompt, timeout):
-#     # set signal handler
-#     signal.signal(signal.SIGALRM, alarm_handler)
-#     signal.alarm(timeout)  # produce SIGALRM in `timeout` seconds
-#
-#     try:
-#         return readchar()
-#     finally:
-#         signal.alarm(0)  # cancel alarm
-#
-# def waitForKeyPress2(function=None, timeout=5, message='Waiting ... ' ):
-#
-#     message = message + '\npress ' + Fore.BLUE + Style.BRIGHT + '"c"' + Style.RESET_ALL + ' to continue or ' + Fore.BLUE + Style.BRIGHT + '"q"' + Style.RESET_ALL + ' to abort.'
-#
-#     # while True:
-#     #     with keyboard.Events() as events:
-#     #         if not function is None:
-#     #             print('Calling function')
-#     #             function()
-#     #
-#     #         print(message)
-#     #         event = events.get(timeout)
-#     #         if not event is None:
-#     #             if hasattr(event.key, 'char'):
-#     #                 if event.key.char == 'c':
-#     #                     print('\nyou pressed c ... continuing.')
-#     #                     break
-#     #                 elif event.key.char == 'q':
-#     #                     print('\nyou pressed q ... aborting.')
-#     #                     exit(0)
+def raise_timeout_error(signum, frame):
+    raise subprocess.TimeoutExpired(None, 1)
 
 
-def waitForKeyPress(function=None, timeout=5, message='Waiting ... ' ):
+def input_with_timeout(prompt, timeout):
+    # set signal handler
+    signal(SIGALRM, raise_timeout_error)
+    setitimer(ITIMER_REAL, timeout)
 
+    try:
+        k = readchar.readkey()
+        print('received key')
+        return k
+    except:
+        # print('Timeout')
+        pass
+    finally:
+        # print('finally')
+        # signal.alarm(0)  # cancel alarm
+        setitimer(ITIMER_REAL, 0)
+
+
+def waitForKeyPress2(function=None, timeout=5, message='Waiting ... '):
+    message = message + '\npress ' + Fore.BLUE + Style.BRIGHT + '"c"' + Style.RESET_ALL + ' to continue or ' + Fore.BLUE + Style.BRIGHT + '"q"' + Style.RESET_ALL + ' to abort.'
+
+    while True:
+        if not function is None:
+            print('Calling function')
+            function()
+
+        print(message)
+        key = input_with_timeout('', 1)
+        print('key: ' + str(key))
+        if key == 'c':
+            print('\nyou pressed c ... continuing.')
+            break
+        elif key == 'q':
+            print('\nyou pressed q ... aborting.')
+            exit(0)
+
+
+def waitForKeyPress(function=None, timeout=5, message='Waiting ... '):
     message = message + '\npress ' + Fore.BLUE + Style.BRIGHT + '"c"' + Style.RESET_ALL + ' to continue or ' + Fore.BLUE + Style.BRIGHT + '"q"' + Style.RESET_ALL + ' to abort.'
 
     while True:
@@ -77,6 +84,7 @@ def waitForKeyPress(function=None, timeout=5, message='Waiting ... ' ):
                     elif event.key.char == 'q':
                         print('\nyou pressed q ... aborting.')
                         exit(0)
+
 
 # Check https://stackoverflow.com/questions/52431265/how-to-use-a-lambda-as-parameter-in-python-argparse
 def create_lambda_with_globals(s):
@@ -101,5 +109,3 @@ def laser_scan_data_to_xy(data):
         y.append(r * math.sin(theta))
 
     return x, y
-
-
