8d51dc1e48c0d9c446f7eb0dd79c798be9e7fd4d
Manuel Gomes <manuelgomes@ua.pt>
2022-05-19 12:25:06 +0100

    #463 bug solved

--- atom_evaluation/scripts/other_calibrations/icp_calibration.py
+++ atom_evaluation/scripts/other_calibrations/icp_calibration.py
@@ -46,7 +46,7 @@ def main():
     T_lidar1_to_lidar2 = getTransform(source_frame, target_frame,
                                       dataset['collections'][selected_collection_key]['transforms'])
 
-
+    print(source_frame)
     print('T_lidar1_to_lidar2 = ' + str(T_lidar1_to_lidar2))
 
     # TODO use ICP for all collections and take average
@@ -67,7 +67,7 @@ def main():
         target_temp = copy.deepcopy(target)
         source_temp.paint_uniform_color([1, 0.706, 0])
         target_temp.paint_uniform_color([0, 0.651, 0.929])
-        source_temp.transform(transformation)
+        target_temp.transform(transformation)
         o3d.visualization.draw_geometries([source_temp, target_temp],
                                           zoom=0.4459,
                                           front=[0.9288, -0.2951, -0.2242],
