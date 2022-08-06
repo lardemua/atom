84435498b5746a40992eece475870598eb43440d
Miguel Oliveira <m.riem.oliveira@gmail.com>
2022-08-06 11:56:01 +0100

    Close #525. Implemented.

--- atom_calibration/src/atom_calibration/dataset_playback/lidar3d_manual_labeling.py
+++ atom_calibration/src/atom_calibration/dataset_playback/lidar3d_manual_labeling.py
@@ -7,6 +7,7 @@ def selectedPointsCallback(selected_point_cloud, selection, dataset):
     """
 
     collection_key = selection['collection_key']
+    print('Adding selected points to idxs labels of collection ' + collection_key + ' ...')
 
     # Extract xyz coordinates from the selected points
     points_selected = pc2.read_points(selected_point_cloud)
@@ -32,12 +33,15 @@ def selectedPointsCallback(selected_point_cloud, selection, dataset):
             dataset['collections'][collection_key]['labels'][sensor]['detected'] == False:
         dataset['collections'][collection_key]['labels'][sensor]['detected'] = True
 
+    print('Done.')
 
 def selectedPointsBorderCallback(selected_point_cloud, selection, dataset):
     """
     Callback function to add selected points to the dataset as idxs_limit_points.
     """
+
     collection_key = selection['collection_key']
+    print('Adding selected points to idxs_limit_points labels of collection ' + collection_key + ' ...')
 
     # Extract xyz coordinates from the selected points
     points_selected = pc2.read_points(selected_point_cloud)
@@ -64,11 +68,14 @@ def selectedPointsBorderCallback(selected_point_cloud, selection, dataset):
     dataset['collections'][collection_key]['labels'][sensor]['idxs_limit_points'] = idx_border
 
 
+    print('Done.')
+
 def selectedPointsRemoveCallback(selected_point_cloud, selection, dataset):
     """
     Callback function to remove selected points from the dataset.
     """
     collection_key = selection['collection_key']
+    print('Removing selected points from labels of collection ' + collection_key + ' ...')
 
     # Extract xyz coordinates from the selected points
     points_selected = pc2.read_points(selected_point_cloud)
@@ -95,11 +102,15 @@ def selectedPointsRemoveCallback(selected_point_cloud, selection, dataset):
     dataset['collections'][collection_key]['labels'][sensor]['idxs'] = idx_center_new
     dataset['collections'][collection_key]['labels'][sensor]['idxs_limit_points'] = idx_border_new
 
+    print('Done.')
 
 def selectedPointsClearAllCallback(selected_point_cloud, selection, dataset):
     """
     Callback function to remove all points of the selected sensor from the dataset.
     """
+
+    print('Clearing all labels of collection ' + collection_key + ' ...')
+
     collection_key = selection['collection_key']
     # Extract xyz coordinates from the selected points
     points_selected = pc2.read_points(selected_point_cloud)
@@ -114,3 +125,5 @@ def selectedPointsClearAllCallback(selected_point_cloud, selection, dataset):
 
     dataset['collections'][collection_key]['labels'][sensor]['idxs'] = []
     dataset['collections'][collection_key]['labels'][sensor]['idxs_limit_points'] = []
+
+    print('Done.')
