1603e3cc61650104f269a42b911c422787c930a4
Miguel Oliveira <m.riem.oliveira@gmail.com>
2023-12-28 09:18:17 +0000

    #775 nesting params in yaml results file

--- atom_core/src/atom_core/results_yml_io.py
+++ atom_core/src/atom_core/results_yml_io.py
@@ -27,6 +27,7 @@ def saveResultsYml(dataset, selected_collection_key, output_file):
 
     # Cycle all sensors in calibration config, and for each replace the optimized transform in the original xacro
     # Parse xacro description file
+    d = {}
     for sensor_key, sensor in dataset["calibration_config"]['sensors'].items():
 
         # Search for corresponding transform. Since this is a sensor transformation it must be static, which is why we use only one collection, the selected collection key
@@ -38,13 +39,14 @@ def saveResultsYml(dataset, selected_collection_key, output_file):
                 quat = transform['quat']
                 rpy = tf.transformations.euler_from_quaternion(quat, axes='rxyz')
 
-                dict_yml[sensor_key + '_x'] = trans[0]
-                dict_yml[sensor_key + '_y'] = trans[1]
-                dict_yml[sensor_key + '_z'] = trans[2]
+                d[sensor_key] = {}
+                d[sensor_key]['x'] = float(trans[0])
+                d[sensor_key]['y'] = float(trans[1])
+                d[sensor_key]['z'] = float(trans[2])
 
-                dict_yml[sensor_key + '_roll'] = rpy[0]
-                dict_yml[sensor_key + '_pitch'] = rpy[1]
-                dict_yml[sensor_key + '_yaw'] = rpy[2]
+                d[sensor_key]['roll'] = float(rpy[0])
+                d[sensor_key]['pitch'] = float(rpy[1])
+                d[sensor_key]['yaw'] = float(rpy[2])
                 found = True
                 break
 
@@ -52,8 +54,11 @@ def saveResultsYml(dataset, selected_collection_key, output_file):
             raise atomError("Could not find transform for sensor " + sensor_key +
                             '. Cannot produce yaml file with calibration results.')
 
+    dict_yml['sensors'] = d
+
     # Cycle all additional_tfs in calibration config, and for each replace the optimized transform in the original xacro
     # Parse xacro description file
+    d = {}
     if dataset['calibration_config']['additional_tfs'] is not None:
         for additional_tf_key, additional_tf in dataset["calibration_config"]['additional_tfs'].items():
 
@@ -66,13 +71,13 @@ def saveResultsYml(dataset, selected_collection_key, output_file):
                     quat = transform['quat']
                     rpy = tf.transformations.euler_from_quaternion(quat, axes='rxyz')
 
-                    dict_yml[sensor_key + '_x'] = trans[0]
-                    dict_yml[sensor_key + '_y'] = trans[1]
-                    dict_yml[sensor_key + '_z'] = trans[2]
+                    d[sensor_key + '_x'] = float(trans[0])
+                    d[sensor_key + '_y'] = float(trans[1])
+                    d[sensor_key + '_z'] = float(trans[2])
 
-                    dict_yml[sensor_key + '_roll'] = rpy[0]
-                    dict_yml[sensor_key + '_pitch'] = rpy[1]
-                    dict_yml[sensor_key + '_yaw'] = rpy[2]
+                    d[sensor_key + '_roll'] = float(rpy[0])
+                    d[sensor_key + '_pitch'] = float(rpy[1])
+                    d[sensor_key + '_yaw'] = float(rpy[2])
                     found = True
                     break
 
@@ -80,8 +85,11 @@ def saveResultsYml(dataset, selected_collection_key, output_file):
                 raise atomError("Could not find transform for additional_tf " + additional_tf_key +
                                 '. Cannot produce yaml file with calibration results.')
 
+        dict_yml['additional_tfs'] = d
+
     # Cycle all joints in calibration config, and for each replace the optimized transform in the original xacro
     # Parse xacro description file
+    d = {}
     if dataset['calibration_config']['joints'] is not None:
         for config_joint_key, config_joint in dataset["calibration_config"]['joints'].items():
 
@@ -91,9 +99,10 @@ def saveResultsYml(dataset, selected_collection_key, output_file):
 
                 if config_joint_key == joint_key:
 
+                    d[joint_key] = {}
                     for param_to_calibrate in config_joint['params_to_calibrate']:
                         calibrated_value = dataset['collections'][selected_collection_key]['joints'][joint_key][param_to_calibrate]
-                        dict_yml[config_joint_key + '_' + param_to_calibrate] = calibrated_value
+                        d[joint_key][param_to_calibrate] = float(calibrated_value)
 
                     found = True
                     break
@@ -102,10 +111,11 @@ def saveResultsYml(dataset, selected_collection_key, output_file):
                 raise atomError("Could not find transform for additional_tf " + additional_tf_key +
                                 '. Cannot produce yaml file with calibration results.')
 
+        dict_yml['joints'] = d
     # Make sure all values in dict are float
     # https://stackoverflow.com/questions/21695705/dump-an-python-object-as-yaml-file
-    for key in dict_yml.keys():
-        dict_yml[key] = float(dict_yml[key])
+    # for key in dict_yml.keys():
+    #     dict_yml[key] = float(dict_yml[key])
 
     # DEBUG
     # for key in dict_yml.keys():  # just to verify
@@ -113,4 +123,4 @@ def saveResultsYml(dataset, selected_collection_key, output_file):
     # print(dict_yml)
 
     print('Saved calibrated parameters to yaml file ' + Fore.BLUE + output_file + Style.RESET_ALL)
-    yaml.dump(dict_yml, open(output_file, 'w'))
+    yaml.dump(dict_yml, open(output_file, 'w'), sort_keys=False)
