69c80ada651690762a09a1917d69341fb4bfb5da
Miguel Oliveira <m.riem.oliveira@gmail.com>
2024-03-30 15:55:51 +0000

    #912 opencv function running

--- atom_evaluation/scripts/other_calibrations/cv_handeye_calib.py
+++ atom_evaluation/scripts/other_calibrations/cv_handeye_calib.py
@@ -23,23 +23,117 @@ from atom_core.dataset_io import saveAtomDataset
 from atom_core.geometry import traslationRodriguesToTransform
 
 
+# def cvHandEyeCalibrate(objp, dataset, camera, pattern, number_of_corners):
+#     # Arrays to store object points and image points from all the images.
+#     objpoints = []  # 3d point in real world space
+#     imgpoints_camera = []  # 2d points in image plane.
+#
+#     for collection_key, collection in dataset['collections'].items():
+#
+#         tmp_imgpoints_camera = np.ones((number_of_corners, 2), np.float32)  # temporary var
+#
+#         for idx, point in enumerate(collection['labels'][pattern][camera]['idxs']):
+#             tmp_imgpoints_camera[idx, 0] = point['x']
+#             tmp_imgpoints_camera[idx, 1] = point['y']
+#
+#         imgpoints_camera.append(tmp_imgpoints_camera)
+#         objpoints.append(objp)  # I don't understand this
+#
+#     # print(imgpoints_camera)
+#
+#     # ---------------------------------------
+#     # --- Get intrinsic data for the sensor
+#     # ---------------------------------------
+#     # Source sensor
+#     K = np.zeros((3, 3), np.float32)
+#     D = np.zeros((5, 1), np.float32)
+#     K[0, :] = dataset['sensors'][camera]['camera_info']['K'][0:3]
+#     K[1, :] = dataset['sensors'][camera]['camera_info']['K'][3:6]
+#     K[2, :] = dataset['sensors'][camera]['camera_info']['K'][6:9]
+#     D[:, 0] = dataset['sensors'][camera]['camera_info']['D'][0:5]
+#
+#     height = dataset['sensors'][camera]['camera_info']['height']
+#     width = dataset['sensors'][camera]['camera_info']['width']
+#     image_size = (height, width)
+#
+#     #############################
+#     # Undistort detection points
+#     #############################
+#
+#     print(f'Calculating undistorted corner detection coordinates...')
+#     undistorted_imgpoints_camera = []  # Init matrix with points for every collection
+#
+#     for i in range(0, len(imgpoints_camera)):  # Iterate through the collections
+#         tmp_undistorted_imgpoints_camera = cv2.undistortPoints(imgpoints_camera[i], K, D)
+#         undistorted_imgpoints_camera.append(tmp_undistorted_imgpoints_camera)
+#
+#     #####################################
+#     # Get transform from target to cam
+#     #####################################
+#
+#     # NOTE: The documentation calls this the transform from world to cam because in their example (which is eye-in-hand), their pattern's frame is their world frame. This is not the case in ours. So when the documentation mentions the world to cam tf, it corresponds to our pattern/target to camera tf. (I think.)
+#
+#     # Use solvePnP() to get this transformation
+#     # Need to check if these calculations (from here to l. 108) are correct
+#
+#     print('Calculating transform from camera to pattern for collection 006....')
+#
+#     _, rvec, tvec = cv2.solvePnP(objp, undistorted_imgpoints_camera[0], K, D)
+#
+#     print(f'rvec = {rvec}')
+#
+#     print(f'tvec = {tvec}')
+#
+#     # Convert it into an homogenous transform
+#     cam_optical_frame_T_pattern = traslationRodriguesToTransform(tvec, rvec)
+#
+#     print(f'Transform from the camera\'s optical frame to the pattern frame = {cam_optical_frame_T_pattern}')
+#
+#     # Get tf from camera's optical frame to camera frame
+#     tmp_transforms = dataset['collections']['006']['transforms']
+#     cam_T_cam_optical_frame = getTransform('rgb_world_link', 'rgb_world_optical_frame', tmp_transforms)
+#
+#     print(f'Transform from cam to cam optical = {cam_T_cam_optical_frame}')
+#
+#     cam_T_pattern = np.dot(cam_T_cam_optical_frame, cam_optical_frame_T_pattern)
+#
+#     print(f'Transform from camera to pattern = {cam_T_pattern}')
+#
+#     # I think I need to invert the transform matrix
+#     pattern_T_cam = np.linalg.inv(cam_T_pattern)
+#
+#     # Split into R and t matrices for the calibration function
+#     pattern_R_cam = pattern_T_cam[0:3, 0:3]
+#     pattern_t_cam = (pattern_T_cam[0:3, 3]).T
+#
+#     ################################################
+#     # Get transform from base to gripper
+#     ################################################
+#
+#     # Hard coded for now, testin w/ one collection before iterating through all collections
+#     base_T_gripper = getTransform('base_link', 'flange', tmp_transforms)
+#
+#     # Split into R and t matrices for the calibration function
+#     base_R_gripper = base_T_gripper[0:3, 0:3]
+#     base_t_gripper = (base_T_gripper[0:3, 3]).T
+#
+#     ################################################
+#     # Calibrate
+#     ################################################
+#
+#     print('----------------\n\n\n\n')
+#     print(pattern_R_cam)
+#     print(pattern_R_cam.shape)
+#
+#     # Running with lists of np.arrays
+#
+#     # cv2.calibrateRobotWorldHandEye(pattern_R_cam, pattern_t_cam, base_R_gripper,
+#     #                                base_t_gripper)  # THIS IS PRODUCING AN ERROR, SEE ISSUE #912
+#
+#     exit(0)
+
+
 def cvHandEyeCalibrate(objp, dataset, camera, pattern, number_of_corners):
-    # Arrays to store object points and image points from all the images.
-    objpoints = []  # 3d point in real world space
-    imgpoints_camera = []  # 2d points in image plane.
-
-    for collection_key, collection in dataset['collections'].items():
-        
-        tmp_imgpoints_camera = np.ones((number_of_corners, 2), np.float32) # temporary var
-
-        for idx, point in enumerate(collection['labels'][pattern][camera]['idxs']):
-            tmp_imgpoints_camera[idx, 0] = point['x']
-            tmp_imgpoints_camera[idx, 1] = point['y']
-
-        imgpoints_camera.append(tmp_imgpoints_camera)
-        objpoints.append(objp)            #### I don't understand this
-
-    # print(imgpoints_camera)
 
     # ---------------------------------------
     # --- Get intrinsic data for the sensor
@@ -56,78 +150,99 @@ def cvHandEyeCalibrate(objp, dataset, camera, pattern, number_of_corners):
     width = dataset['sensors'][camera]['camera_info']['width']
     image_size = (height, width)
 
-    #############################
-    # Undistort detection points
-    #############################
+    # iterate all collections and create four lists of rotations and translations:
+    list_cam_optical_frame_T_pattern_R = []
+    list_cam_optical_frame_T_pattern_t = []
+    list_base_T_gripper_R = []
+    list_base_T_gripper_t = []
 
-    print(f'Calculating undistorted corner detection coordinates...')
-    undistorted_imgpoints_camera = [] # Init matrix with points for every collection
-    
-    for i in range(0, len(imgpoints_camera)): # Iterate through the collections
-        tmp_undistorted_imgpoints_camera = cv2.undistortPoints(imgpoints_camera[i], K, D)
-        undistorted_imgpoints_camera.append(tmp_undistorted_imgpoints_camera)
+    for collection_key, collection in dataset['collections'].items():
 
-    #####################################
-    # Get transform from target to cam
-    #####################################
+        # ---------------------------------------
+        # Get transform from target to cam
+        # ---------------------------------------
 
-    # NOTE: The documentation calls this the transform from world to cam because in their example (which is eye-in-hand), their pattern's frame is their world frame. This is not the case in ours. So when the documentation mentions the world to cam tf, it corresponds to our pattern/target to camera tf. (I think.)
+        # Arrays to store object points and image points from all the images.
+        objpoints = []  # 3d point in real world space
+        imgpoints_camera = []  # 2d points in image plane.
 
-    # Use solvePnP() to get this transformation
-    # Need to check if these calculations (from here to l. 108) are correct
+        tmp_imgpoints_camera = np.ones((number_of_corners, 2), np.float32)  # temporary var
 
-    print('Calculating transform from camera to pattern for collection 006....')
+        for idx, point in enumerate(collection['labels'][pattern][camera]['idxs']):
+            tmp_imgpoints_camera[idx, 0] = point['x']
+            tmp_imgpoints_camera[idx, 1] = point['y']
 
-    _, rvec, tvec = cv2.solvePnP(objp, undistorted_imgpoints_camera[0], K, D)
+        imgpoints_camera.append(tmp_imgpoints_camera)
+        objpoints.append(objp)  # Use the points defined in local pattern ref frame
 
-    print(f'rvec = {rvec}')
+        # print(imgpoints_camera)
 
-    print(f'tvec = {tvec}')
+        # ---------------------------------------
+        # Undistort detection points
+        # ---------------------------------------
 
-    # Convert it into an homogenous transform
-    cam_optical_frame_T_pattern = traslationRodriguesToTransform(tvec,rvec)
-    
-    print(f'Transform from the camera\'s optical frame to the pattern frame = {cam_optical_frame_T_pattern}')
+        print(f'Calculating undistorted corner detection coordinates...')
+        undistorted_imgpoints_camera = []  # Init matrix with points for every collection
 
-    # Get tf from camera's optical frame to camera frame
-    tmp_transforms = dataset['collections']['006']['transforms']
-    cam_T_cam_optical_frame = getTransform('rgb_world_link','rgb_world_optical_frame', tmp_transforms)
+        for i in range(0, len(imgpoints_camera)):  # Iterate through the collections
+            tmp_undistorted_imgpoints_camera = cv2.undistortPoints(imgpoints_camera[i], K, D)
+            undistorted_imgpoints_camera.append(tmp_undistorted_imgpoints_camera)
 
-    print(f'Transform from cam to cam optical = {cam_T_cam_optical_frame}')
+        # ---------------------------------------
+        # Get transform from target to cam
+        # ---------------------------------------
+        # NOTE: The documentation calls this the transform from world to cam because in their example (which is eye-in-hand), their pattern's frame is their world frame. This is not the case in ours. So when the documentation mentions the world to cam tf, it corresponds to our pattern/target to camera tf. (I think.)
 
-    cam_T_pattern = np.dot(cam_T_cam_optical_frame, cam_optical_frame_T_pattern)
+        # Use solvePnP() to get this transformation
+        # Need to check if these calculations (from here to l. 108) are correct
 
-    print(f'Transform from camera to pattern = {cam_T_pattern}')
+        print('Calculating transform from camera to pattern for collection ' + collection_key)
 
+        _, rvec, tvec = cv2.solvePnP(objp, undistorted_imgpoints_camera[0], K, D)
 
-    # I think I need to invert the transform matrix
-    pattern_T_cam = np.linalg.inv(cam_T_pattern)
-    
-    #Split into R and t matrices for the calibration function
-    pattern_R_cam = pattern_T_cam[0:3, 0:3]
-    pattern_t_cam = (pattern_T_cam[0:3, 3]).T
+        print(f'rvec = {rvec}')
+        print(f'tvec = {tvec}')
 
+        # Convert it into an homogenous transform
+        cam_optical_frame_T_pattern = traslationRodriguesToTransform(tvec, rvec)
 
-    ################################################
-    # Get transform from base to gripper
-    ################################################
-    
-    # Hard coded for now, testin w/ one collection before iterating through all collections
-    base_T_gripper = getTransform('base_link', 'flange', tmp_transforms)
+        # NOTE fstrings are forbidden in atom :-)
+        print(f'cam_optical_frame_T_pattern = {cam_optical_frame_T_pattern}')
 
-    # Split into R and t matrices for the calibration function
-    base_R_gripper = base_T_gripper[0:3,0:3]
-    base_t_gripper = (base_T_gripper[0:3, 3]).T
+        # NOTE: you do not need to compute the transform from camera to camera_optical_frame
 
+        # Split into R and t matrices for the calibration function
+        cam_optical_frame_T_pattern_R = cam_optical_frame_T_pattern[0:3, 0:3]
+        cam_optical_frame_T_pattern_t = (cam_optical_frame_T_pattern[0:3, 3]).T
 
-    ################################################
+        list_cam_optical_frame_T_pattern_R.append(cam_optical_frame_T_pattern_R)
+        list_cam_optical_frame_T_pattern_t.append(cam_optical_frame_T_pattern_t)
+
+        # ---------------------------------------
+        # Get transform from base to gripper
+        # ---------------------------------------
+
+        # Hard coded for now, testin w/ one collection before iterating through all collections
+        # NOTE: cannot test with a single collection. We need at least three. Check
+        # https://docs.opencv.org/4.x/d9/d0c/group__calib3d.html#ga41b1a8dd70eae371eba707d101729c36
+
+        base_T_gripper = getTransform('base_link', 'flange', collection['transforms'])
+
+        # Split into R and t matrices for the calibration function
+        base_T_gripper_R = base_T_gripper[0:3, 0:3]
+        base_T_gripper_t = (base_T_gripper[0:3, 3]).T
+
+        list_base_T_gripper_R.append(base_T_gripper_R)
+        list_base_T_gripper_t.append(base_T_gripper_t)
+
+    # ---------------------------------------
     # Calibrate
-    ################################################
+    # ---------------------------------------
 
-    cv2.calibrateRobotWorldHandEye(pattern_R_cam, pattern_t_cam, base_R_gripper, base_t_gripper) # THIS IS PRODUCING AN ERROR, SEE ISSUE #912
-
-    exit(0)
-
+    # Running with lists of np.arrays
+    cv2.calibrateRobotWorldHandEye(list_cam_optical_frame_T_pattern_R,
+                                   list_cam_optical_frame_T_pattern_t,
+                                   list_base_T_gripper_R, list_base_T_gripper_t)
 
 
 def getPatternConfig(dataset, pattern):
@@ -137,7 +252,8 @@ def getPatternConfig(dataset, pattern):
     square = dataset['calibration_config']['calibration_patterns'][pattern]['size']
     inner_square = dataset['calibration_config']['calibration_patterns'][pattern]['inner_size']
     objp = np.zeros((nx * ny, 3), np.float32)
-    objp[:, :2] = square * np.mgrid[0:nx, 0:ny].T.reshape(-1, 2) # set of coordinates (w.r.t. the pattern frame) of the corners 
+    # set of coordinates (w.r.t. the pattern frame) of the corners
+    objp[:, :2] = square * np.mgrid[0:nx, 0:ny].T.reshape(-1, 2)
 
     return nx, ny, square, inner_square, objp
 
@@ -157,11 +273,9 @@ def main():
     show_images = args['show_images']
     pattern = args['pattern']
 
-
     # Read json file
     f = open(json_file, 'r')
     dataset = json.load(f)
-    
 
     #########################################
     # DATASET PREPROCESSING
@@ -171,7 +285,7 @@ def main():
 
     # Remove partial detections (OpenCV does not support them)
     collections_to_delete = []
-    
+
     number_of_corners = int(nx) * int(ny)
     for collection_key, collection in dataset['collections'].items():
         for sensor_key, sensor in dataset['sensors'].items():
@@ -185,7 +299,7 @@ def main():
                         collection_key + ', sensor ' + sensor_key)
 
                     collections_to_delete.append(collection_key)
-                    break    
+                    break
 
     for collection_key in collections_to_delete:
         del dataset['collections'][collection_key]
@@ -201,16 +315,15 @@ def main():
         del dataset['collections'][collection_key]
 
     print('\nUsing ' + str(len(dataset['collections'])) + ' collections.')
-    
-    # # Compute OpenCV stereo calibration
+
+    # Compute OpenCV stereo calibration
+    # Now does not crash!!!
     cvHandEyeCalibrate(objp=objp, dataset=dataset, camera=camera, pattern=pattern, number_of_corners=number_of_corners)
+
 
 if __name__ == '__main__':
 
     main()
-    
-
-
 
     # R = calib_model['R']
     # t = calib_model['T']
