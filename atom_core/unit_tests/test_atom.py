189477a1c58ae3b8f7ca875535afe7007ee3e273
Kazadhum <diogoscsv@gmail.com>
2025-02-27 01:17:12 +0000

    feat(unittest): added simple test for getAggregateTransform
    Added a simple test for getAggregateTransform that tests whether a chain
    with only "null" transformations will result in an identity matrix being
    returned; Additionally, moved the input for the unit tests to a separate
    folder and files.

--- atom_core/unit_tests/test_atom.py
+++ atom_core/unit_tests/test_atom.py
@@ -1,4 +1,7 @@
 import unittest
+import json
+
+import numpy as np
 from atom_core.atom import getChain, getAggregateTransform, getTransform
 
 class TestGetChain(unittest.TestCase):
@@ -8,14 +11,9 @@ class TestGetChain(unittest.TestCase):
         from_frame = 'frame_0'
         to_frame = 'frame_3'
 
-        # Create an example tranform pool
-        transform_pool = {} # Init the tranform_pool variable
-
-        for i in range(0,10):
-            parent_frame = 'frame_' + str(i)
-            child_frame = 'frame_' + str(i+1)
-            transform_pool[parent_frame + '-' + child_frame] = {'parent': parent_frame,
-                                                                'child': child_frame}
+        # Open testing transform pool
+        with open('test_inputs/atom_000.json') as f:
+            transform_pool = json.load(f)
 
         # Call the function
         func_result = getChain(from_frame, to_frame, transform_pool)
@@ -40,55 +38,10 @@ class TestGetChain(unittest.TestCase):
         from_frame = 'frame_0'
         to_frame = 'frame_7'
 
-        # Hardcode in an example transform pool with "branches"
-
-        transform_pool = {
-            'frame_0-frame_1': {
-                'parent': 'frame_0',
-                'child': 'frame_1'
-            },
-            'frame_1-frame_2': {
-                'parent': 'frame_1',
-                'child': 'frame_2'
-            },
-            # 3 branches from frame_2
-            'frame_2-frame_3': {
-                'parent': 'frame_2',
-                'child': 'frame_3'
-            },
-            'frame_2-frame_4': {
-                'parent': 'frame_2',
-                'child': 'frame_4'
-            },
-            'frame_2-frame_5': {
-                'parent': 'frame_2',
-                'child': 'frame_5'
-            },
-            # 2 branches from frame_3
-            'frame_3-frame_6': {
-                'parent': 'frame_3',
-                'child': 'frame_6'
-            },
-            'frame_3-frame_7': {
-                'parent': 'frame_3',
-                'child': 'frame_7'
-            },
-            # 1 branch from frame_4
-            'frame_4-frame_8': {
-                'parent': 'frame_4',
-                'child': 'frame_8'
-            },
-            # 2 branches from frame_5
-            'frame_5-frame_9': {
-                'parent': 'frame_5',
-                'child': 'frame_9'
-            },
-            'frame_5-frame_10': {
-                'parent': 'frame_5',
-                'child': 'frame_10'
-            }
-        }
-
+        # Open testing transform pool
+        with open('test_inputs/atom_001.json') as f:
+            transform_pool = json.load(f)
+        
         # Call the function
         func_result = getChain(from_frame, to_frame, transform_pool)
 
@@ -111,9 +64,31 @@ class TestGetChain(unittest.TestCase):
         self.assertEqual(correct_result, func_result)
 
 
-# class TestGetAggregateTransform(unittest.TestCase):
-#     def test_get_aggregate_transforms(self):
-#         pass
+class TestGetAggregateTransform(unittest.TestCase):
+    def test_get_aggregate_transforms_eye_x_eye(self):
+        # A simple sanity check
+        
+        with open('test_inputs/atom_002.json') as f:
+            transform_pool = json.load(f)
+
+        chain = [
+            {'parent': 'frame_0',
+             'child': 'frame_1',
+             'key': 'frame_0-frame_1'},
+            {'parent': 'frame_1',
+             'child': 'frame_2',
+             'key': 'frame_1-frame_2'},
+            {'parent': 'frame_2',
+            'child': 'frame_3',
+            'key': 'frame_2-frame_3'}
+        ]
+
+        correct_result = np.eye(4, dtype=float)
+
+        func_result = getAggregateTransform(chain, transform_pool)
+
+        np.testing.assert_array_equal(correct_result, func_result)
+
         
 # class TestGetTransform(unittest.TestCase):
 #     def test_get_transform(self):
