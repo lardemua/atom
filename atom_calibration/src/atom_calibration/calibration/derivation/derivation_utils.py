fix(tf_derivator): fixed rotation derivation #1015
Now derives using rotation matrices instead of quaternions

atom_calibration/src/atom_calibration/calibration/derivation/derivation_utils.py
@@ -12,6 +12,7 @@ from atom_core.utilities import atomError
 from matplotlib import pyplot as plt
 from scipy.spatial.transform import Rotation
 
+
 def plotIMUData(dataset: Dict) -> None:
     """Simple function for debugging."""
 
@@ -44,6 +45,7 @@ def plotIMUData(dataset: Dict) -> None:
 
     plt.show()
 
+
 def timeFloatToStamp(t_float: float) -> Dict[str, int]:
 
     secs = floor(t_float)
@@ -52,7 +54,7 @@ def timeFloatToStamp(t_float: float) -> Dict[str, int]:
     stamp = {"secs": secs, "nsecs": nsecs}
 
     return stamp
-    
+
 
 def timeStampToFloat(stamp: Dict[str, int]) -> float:
 
@@ -146,102 +148,6 @@ def getTFList(dataset: Dict) -> List[Dict]:
         tf_list.append(tf_dict_to_append)
 
     return tf_list
-
-
-def centralNumericalFirstDerivative(data: Dict[str, float]) -> List[float]:
-    """
-    Numerically calculates the first derivative at instant t_i using central derivation.
-
-    Input:
-        data: a dictionary of the relevant data for derivation. The dictionary must have the following structure:
-
-        data = {
-            "t": [
-                t_{i-1},
-                t_i,
-                t_{i+1},
-                ],
-            "var1": [
-                var1(t_{i-1}),
-                var(t_i),
-                var(t_{i+1}),
-                ],
-            ....
-            "varN": [
-                varN(t_{i-1}),
-                varN(t_i),
-                varN(t_{i+1}),
-                ]
-            }
-
-    Output:
-        ddata_dt: a list with the derivatives at instant t, in order of the variables of the input dictionary
-
-        ddata_dt = [
-            dvar1_dt(t_i),
-            ...,
-            dvarN_dt(t_i)
-            ]
-    """
-    ddata_dt = []
-
-    for var in data.keys():
-        if var == "t":
-            continue
-
-        dvar_dt = (data[var][2] - data[var][0]) / (data["t"][2] - data["t"][0])
-        ddata_dt.append(dvar_dt)
-
-    return ddata_dt
-
-
-def centralNumericalSecondDerivative(data: Dict[str, float]) -> List[float]:
-    """
-    Numerically calculates the second derivative at instant t_i using central derivation.
-
-    Input:
-        data: a dictionary of the relevant data for derivation. Uses 3 datapoints, where t_i is the central instant. The dictionary must have the following structure:
-
-        data = {
-            "t": [
-                t_{i-1},
-                t_i,
-                t_{i+1},
-                ],
-            "var1": [
-                var1(t_{i-1}),
-                var(t_i),
-                var(t_{i+1}),
-                ],
-            ....
-            "varN": [
-                varN(t_{i-1}),
-                varN(t_i),
-                varN(t_{i+1}),
-                ]
-            }
-
-    Output:
-        dddata_dtt: a list with the derivatives at instant t, in order of the variables of the input dictionary
-
-        dddata_dtt = [
-            ddvar1_dtt(t_i),
-            ...,
-            ddvarN_dtt(t_i)
-            ]
-    """
-    dddata_dtt = []
-
-    for var in data.keys():
-        if var == "t":
-            continue
-
-        ddvar_dtt = (data[var][2] - 2 * data[var][1] + data[var][0]) / (
-            (data["t"][2] - data["t"][1]) ** 2
-        )
-        dddata_dtt.append(ddvar_dtt)
-
-    return dddata_dtt
 
 
 def identifyTransitionPoints(tf_list: List, from_frame: str, to_frame: str) -> List:
@@ -443,6 +349,7 @@ def plotDerivationResults(
     to_frame: str,
 ) -> None:
 
+    # NOTE: It doesn't make sense to plot out orientation since it's expressed in quaternions
     data_dict = {
         "t": [],
         "t_reparam": [],
@@ -450,6 +357,8 @@ def plotDerivationResults(
         "lin_vel": {"x": [], "y": [], "z": []},
         "lin_accel": {"x": [], "y": [], "z": []},
         "lin_accel_imu": {"x": [], "y": [], "z": []},
+        "ang_vel": {"x": [], "y": [], "z": []},
+        "ang_vel_imu": {"x": [], "y": [], "z": []},
     }
 
     # Copy it so the original keeps the timestamps
@@ -474,16 +383,21 @@ def plotDerivationResults(
             ),
         )
 
+        # Get data from IMU
         imu_accel = [
             closest_imu_datapoint["linear_acceleration"]["x"],
             closest_imu_datapoint["linear_acceleration"]["y"],
             closest_imu_datapoint["linear_acceleration"]["z"],
         ]
 
+        imu_ang_vel = [
+            closest_imu_datapoint["angular_velocity"]["x"],
+            closest_imu_datapoint["angular_velocity"]["y"],
+            closest_imu_datapoint["angular_velocity"]["z"],
+        ]
+
         R = world_imu_tf[:3, :3]
-
         imu_accel = R @ imu_accel
-
         imu_accel[2] -= 9.81
 
         # For plotting
@@ -501,6 +415,10 @@ def plotDerivationResults(
         data_dict["lin_accel_imu"]["y"].append(imu_accel[1])
         data_dict["lin_accel_imu"]["z"].append(imu_accel[2])
 
+        data_dict["ang_vel_imu"]["x"].append(imu_ang_vel[0])
+        data_dict["ang_vel_imu"]["y"].append(imu_ang_vel[1])
+        data_dict["ang_vel_imu"]["z"].append(imu_ang_vel[2])
+
     # Linear Velocity Data
     data_dict["lin_vel"]["x"] = derivation_results["lin_vel"]["x"]
     data_dict["lin_vel"]["y"] = derivation_results["lin_vel"]["y"]
@@ -510,6 +428,11 @@ def plotDerivationResults(
     data_dict["lin_accel"]["x"] = derivation_results["lin_accel"]["x"]
     data_dict["lin_accel"]["y"] = derivation_results["lin_accel"]["y"]
     data_dict["lin_accel"]["z"] = derivation_results["lin_accel"]["z"]
+
+    # Angular Velocity Data
+    data_dict["ang_vel"]["x"] = derivation_results["ang_vel"]["x"]
+    data_dict["ang_vel"]["y"] = derivation_results["ang_vel"]["y"]
+    data_dict["ang_vel"]["z"] = derivation_results["ang_vel"]["z"]
 
     # Plot x data
     fig1, ax1 = plt.subplots()
@@ -619,11 +542,68 @@ def plotDerivationResults(
         ax=ax9,
     )
 
+    # Angular Velocity Plots
+    fig4, ax10 = plt.subplots()
+    sns.scatterplot(
+        x=data_dict["t_reparam"],
+        y=data_dict["ang_vel"]["x"],
+        marker="o",
+        color="r",
+        label="Derivation Results",
+        ax=ax10,
+    )
+    sns.scatterplot(
+        x=data_dict["t_reparam"],
+        y=data_dict["ang_vel_imu"]["x"],
+        marker="*",
+        color="orange",
+        label="IMU Angular Velocity Data",
+        ax=ax10,
+    )
+
+    fig5, ax11 = plt.subplots()
+    sns.scatterplot(
+        x=data_dict["t_reparam"],
+        y=data_dict["ang_vel"]["y"],
+        marker="o",
+        color="r",
+        label="Derivation Results",
+        ax=ax11,
+    )
+    sns.scatterplot(
+        x=data_dict["t_reparam"],
+        y=data_dict["ang_vel_imu"]["y"],
+        marker="*",
+        color="orange",
+        label="IMU Angular Velocity Data",
+        ax=ax11,
+    )
+    fig6, ax12 = plt.subplots()
+    sns.scatterplot(
+        x=data_dict["t_reparam"],
+        y=data_dict["ang_vel"]["z"],
+        marker="o",
+        color="r",
+        label="Derivation Results",
+        ax=ax12,
+    )
+    sns.scatterplot(
+        x=data_dict["t_reparam"],
+        y=data_dict["ang_vel_imu"]["z"],
+        marker="*",
+        color="orange",
+        label="IMU Angular Velocity Data",
+        ax=ax12,
+    )
     # Some plot formatting
     ax1.set_title(r"Translation Data ($x$)")
     ax4.set_title(r"Translation Data ($y$)")
     ax7.set_title(r"Translation Data ($z$)")
-    for ax in [ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9]:
+    ax10.set_title(r"Angular Velocity Data ($\omega_x$)")
+    ax11.set_title(r"Angular Velocity Data ($\omega_y$)")
+    ax12.set_title(r"Angular Velocity Data ($\omega_z$)")
+
+    for ax in [ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9, ax10]:
         ax.set(xlabel=r"Time since first datapoint, $t$ $[s]$")
     for ax in [ax1, ax4, ax7]:
         ax.set(ylabel=r"Position $[m]$")
@@ -637,10 +617,14 @@ def plotDerivationResults(
         ax.set(ylabel=r"Acceleration $[m/s^2]$")
         ax.set_ylim(-1, 1)
         ax.yaxis.label.set_color("b")
+    for ax in [ax10, ax11, ax12]:
+        ax.set(ylabel=r"Angular Velocity $[rad/s]$")
+        ax.set_ylim(-1.5, 1.5)
+        ax.yaxis.label.set_color("r")
     for fig in [fig1, fig2, fig3]:
         fig.tight_layout()
-
     plt.show()
+
 
 def inspectDerivatives(
     dataset: Dict,

atom_calibration/src/atom_calibration/calibration/derivation/tf_derivator.py
@@ -10,9 +10,9 @@ from copy import deepcopy
 from typing import Any, Dict, List, Tuple
 
 import numpy as np
+from pandas.core.arrays.period import DIFFERENT_FREQ
 import seaborn as sns
 from atom_calibration.calibration.derivation.derivation_utils import (
-    centralNumericalSecondDerivative,
     getTFList,
     getTFToDeriveList,
     inspectDerivatives,
@@ -24,6 +24,7 @@ from atom_calibration.calibration.derivation.derivation_utils import (
 from atom_core.atom import getTransform
 from atom_core.geometry import (
     matrixToTranslationQuaternion,
+    translationQuaternionToTransform,
 )
 from matplotlib import pyplot as plt
 from numpy import float64, generic
@@ -34,67 +35,70 @@ from scipy.signal import savgol_filter
 
 
 def deriveRotation(
-    tf_list: List[Dict[str, Any]], poly_degree: int, visualization: bool
-) -> List[np.poly1d]:
-    """Given a list of transformations, return the functions that describe the angular velocities.
-    Angular velocities are calculated using the following:
-
-        dw = 2 * quaternionMultiplication(dq, q), with dw = [0, wx, wy, wz]
+    tf_data_dict: Dict[str, Any], poly_degree: int, neighbourhood_size: int
+) -> Dict:
+    """
+    Angular velocities are calculated from the temporal derivatives of the rotation matrix
 
     Inputs:
-        - tf_list: a list of transformation dictionaries to use for derivation;
+        - tf_data_dict: a dictionary with the following structure:
+            tf_data_dict = {
+                "t": [t_array],
+                "trans": {"x": [x_trans_array], "y": [y_trans_array], "z": [z_trans_array]},
+                "quat": ...
+                }
+        ;
         - poly_degree: the degree of the polynomial functions to fit the rotation data to;
-        - visualization: enable graph visualization.
+        - neighbourhood_size: number of datapoints to use for the savgol_filter() function.
     Outputs:
-        - p_der: a list of 3 polynomial functions to describe the angular velocity related to each axis of rotation.
+        - ang_vel: A list of 3 arrays of angular velocity at each datapoint;
     """
+
     # Get time values
-    t_arr = np.array([timeStampToFloat(tf["stamp"]) for tf in tf_list])
+    dt = tf_data_dict["t"][1] - tf_data_dict["t"][0]
 
-    # for each rotation variable
-    rot_array = np.array(
-        [
-            [tf["quat"][0] for tf in tf_list],
-            [tf["quat"][1] for tf in tf_list],
-            [tf["quat"][2] for tf in tf_list],
-            [tf["quat"][3] for tf in tf_list],
-        ]
-    )
+    R_arr = []
 
-    q = [np.polyfit(t_arr, rot_array[i], deg=poly_degree) for i in range(4)]
-    dq = []
+    # For each datapoint
+    for i in range(len(tf_data_dict["t"])):
+        quat = [tf_data_dict["quat"][var][i] for var in ["x", "y", "z", "w"]]
+        tvec = [tf_data_dict["trans"][var][i] for var in ["x", "y", "z"]]
 
-    if visualization:
-        fig, axes = plt.subplots(2, 4)
+        # get tf matrix
+        M = translationQuaternionToTransform(tvec, quat)
 
-    # Get quaternion derivatives, dq
-    for i in range(4):
-        poly_func = np.poly1d(q[i])
+        R_arr.append(M[:3, :3])
 
-        dq.append(np.polyder(poly_func))
+    R_arr = np.array(R_arr)
+    dR_arr = np.zeros_like(R_arr)
+    print(np.shape(R_arr))
 
-        if visualization:
-            sns.scatterplot(x=t_arr, y=rot_array[i], ax=axes[0, i])
-            x_func = np.linspace(t_arr.min(), t_arr.max(), 1000)
-            y_func = poly_func(x_func)
-            sns.lineplot(x=x_func, y=y_func, color="red", ax=axes[0, i])
+    # Derive each element of rotation matrix
 
-            yder_func = dq[i](x_func)
-            sns.lineplot(x=x_func, y=yder_func, color="green", ax=axes[1, i])
+    for j in range(3):
+        for k in range(3):
+            series = R_arr[:, j, k]
 
-    q_conjugate = [
-        np.poly1d(q[0]),
-        np.poly1d(-q[1]),
-        np.poly1d(-q[2]),
-        np.poly1d(-q[3]),
-    ]
+            deriv = savgol_filter(
+                x=series,
+                window_length=neighbourhood_size,
+                polyorder=poly_degree,
+                deriv=1,
+                delta=dt,
+            )
 
-    omega = quatMult(2 * dq, q_conjugate)
+            dR_arr[:, j, k] = deriv
 
-    if visualization:
-        plt.show()
+    # Compute ang_vels
+    ang_vels = {"x": [], "y": [], "z": []}
+    for k in range(R_arr.shape[0]):
+        R = R_arr[k]
+        dR = dR_arr[k]
+        omega_hat = R.T @ dR
 
-    ang_vels = [omega[1], omega[2], omega[3]]
+        ang_vels["x"].append(omega_hat[2, 1])
+        ang_vels["y"].append(omega_hat[0, 2])
+        ang_vels["z"].append(omega_hat[1, 0])
 
     return ang_vels
 
@@ -104,8 +108,7 @@ def deriveTranslation(
     poly_degree: int,
     neighbourhood_size: int,
 ) -> Tuple:
-    """Given a list of transformations, return the functions that describe the linear accelerations.
-
+    """
     Inputs:
         - tf_data_dict: a dictionary with the following structure:
             tf_data_dict = {
@@ -268,7 +271,17 @@ def deriveDatasetAllDataPoints(
         neighbourhood_size=neighbourhood_size,
     )
 
-    derivation_results = {"lin_accel": lin_accel_arr, "lin_vel": lin_vel_arr}
+    ang_vels = deriveRotation(
+        tf_data_dict=data_dict,
+        poly_degree=poly_degree,
+        neighbourhood_size=neighbourhood_size,
+    )
+
+    derivation_results = {
+        "lin_accel": lin_accel_arr,
+        "lin_vel": lin_vel_arr,
+        "ang_vel": ang_vels,
+    }
 
     return derivation_results
 
@@ -398,8 +411,6 @@ def calculateErrorsAllDataPoints(
         # e["e_ang_vel"]["z"].append(
         #     imu_ang_vel[2] - results[str(timeStampToFloat(tf_pool_stamp))]["ang_vel"][2]
         # )
-
-        # NOTE: Should I add (and plot out) the rotation? I don't know that it would be super clear, due to the fact that the orientation is expressed in quaternions
 
     fig1, ax1 = plt.subplots()
     sns.scatterplot(