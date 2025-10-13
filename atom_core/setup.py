9c6a9bfefb00f70b8917bbf8ab51dca87d3b1706
Kazadhum <diogoscsv@gmail.com>
2025-10-13 17:52:38 +0100

    chore: fixed catkin_make error by using setuptools instead of distilutils.core.setup

--- atom_core/setup.py
+++ atom_core/setup.py
@@ -1,11 +1,11 @@
 ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['atom_core'],
+    packages=find_packages('src'),
     package_dir={'': 'src'},
 )
 
