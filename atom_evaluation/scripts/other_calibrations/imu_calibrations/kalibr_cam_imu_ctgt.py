da7e9c1640e6e08b53413b3ced732e022d708042
Kazadhum <diogoscsv@gmail.com>
2025-11-24 23:19:37 +0000

    feat: finished kalibr ctgt script

--- atom_evaluation/scripts/other_calibrations/imu_calibrations/kalibr_cam_imu_ctgt.py
+++ atom_evaluation/scripts/other_calibrations/imu_calibrations/kalibr_cam_imu_ctgt.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+from typing import TextIO, Union
 import numpy as np
 from numpy._typing import NDArray
 from atom_core.geometry import matrixToTranslationRotation
@@ -16,9 +17,11 @@ from atom_core.atom import getTransform
 
 def getKalibrTF(file_name: str) -> NDArray:
 
-    file = open(file_name, "r")
+    file: TextIO = open(file_name, "r")
 
-    res_index = None
+    res_index: Union[None, list[int]] = None
+    res_lines: Union[None, list[Union[str, float]]]
+
     for index, line in enumerate(file):
 
         if "T_ci" in line:
@@ -39,21 +42,26 @@ def getKalibrTF(file_name: str) -> NDArray:
 
         res_lines[idx] = [float(x) for x in res_lines[idx] if x != ""]
 
-    kalibr_cam_imu_tf = np.array(res_lines)
+    kalibr_cam_imu_tf: NDArray = np.array(res_lines)
 
     return kalibr_cam_imu_tf
 
 
-def getAtomTf(file_name: str, imu_sensor: str, camera_sensor: str) -> NDArray:
+def getAtomTf(
+    file_name: str,
+    imu_sensor: str,
+    camera_sensor: str,
+    optical_frame_compensation: bool,
+) -> NDArray:
 
     # Read dataset file
-    dataset, json_file = loadResultsJSON(file_name, None)  # 2nd arg is csf
+    dataset, _ = loadResultsJSON(file_name, None)  # 2nd arg is csf
     selected_collection_key = list(dataset["collections"].keys())[0]
 
     tf_pool = dataset["collections"][selected_collection_key]["transforms"]
 
-    source_frame = dataset["sensors"][camera_sensor]["calibration_child"]
-    # source_frame = "hand_rgb"
+    # source_frame = dataset["sensors"][camera_sensor]["calibration_child"]
+    source_frame = "hand_rgb"
     target_frame = dataset["sensors"][imu_sensor]["calibration_child"]
 
     res = getTransform(
@@ -61,6 +69,20 @@ def getAtomTf(file_name: str, imu_sensor: str, camera_sensor: str) -> NDArray:
         to_frame=target_frame,
         transforms=tf_pool,
     )
+
+    if optical_frame_compensation:
+        R = res[:3, :3]
+        M = np.array(
+            [
+                [0, 0, 1],
+                [-1, 0, 0],
+                [0, -1, 0],
+            ]
+        )
+
+        R_of = R @ M
+
+        res[:3, :3] = R_of
 
     return res
 
@@ -95,6 +117,13 @@ def main():
         type=str,
         required=True,
     )
+    ap.add_argument(
+        "-ofc",
+        "--optical_frame_compensation",
+        action="store_true",
+        default=False,
+        help="Rotates camera frame in order to use the typical optical frame representation (Z+ aligned with optical axis). Use if your system does not have the optical frame defined. This might be needed if, for example, you use Webots for simulation.",
+    )
 
     # - Save args
     args = vars(ap.parse_known_args()[0])
@@ -105,6 +134,7 @@ def main():
         file_name=args["json_file"],
         imu_sensor=args["imu_sensor"],
         camera_sensor=args["camera"],
+        optical_frame_compensation=args["optical_frame_compensation"],
     )
     translation_error, rotation_error, _, _, _, _, _, _ = compareTransforms(
         kalibr_res, gt
